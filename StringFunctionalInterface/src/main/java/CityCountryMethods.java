d69f1dcae685a079b280c042f10d37de60aefc26
amrahmed93 <amr.devp@gmail.com>
2021-06-28 21:15:03 +0200

    Updated

--- StringFunctionalInterface/src/main/java/CityCountryMethods.java
+++ StringFunctionalInterface/src/main/java/CityCountryMethods.java
@@ -1,6 +1,7 @@
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
-import java.util.OptionalDouble;
 import java.util.stream.Collectors;
 
 public class CityCountryMethods {
@@ -9,40 +10,63 @@ public class CityCountryMethods {
 
     public CityCountryMethods(List<Country> countries, List<City> cities) {
         this.countries = countries;
-        this.cities = cities;
-    }
+        this.cities = cities; }
 
     public List<Country> getCountries() {
-        return countries;
-    }
+        return countries; }
 
     public void setCountries(List<Country> countries) {
-        this.countries = countries;
-    }
+        this.countries = countries; }
 
     public List<City> getCities() {
-        return cities;
-    }
+        return cities; }
 
     public void setCities(List<City> cities) {
-        this.cities = cities;
-    }
-
+        this.cities = cities; }
     //
+    public HashMap countryCities(){
+        HashMap countryCities=new HashMap();
+        for(Country country:countries){
+            countryCities.put(country.getName(),
+                    cities.stream().filter(city -> city.getCountryCode().equals(" "+country.getCode())).map(City::getName).collect(Collectors.toList()));
+        }return countryCities; }
+
     public List<Integer> populations() {
-        return this.countries.stream().map(Country::getPopulation).collect(Collectors.toList());
-    }
+        return this.countries.stream().map(Country::getPopulation).collect(Collectors.toList()); }
+
 
     public Double averageOfPopulations() {
-        return countries.stream().mapToDouble(Country::getPopulation).average().getAsDouble();
-    }
+        return countries.stream().mapToDouble(Country::getPopulation).average().getAsDouble(); }
+
 
     public Integer maximumPopulation() {
-        return countries.stream().mapToInt(Country::getPopulation).max().getAsInt();
-    }
+        return countries.stream().mapToInt(Country::getPopulation).max().getAsInt(); }
+    
 
     public List<City> sortedCitiesByCountryCode(String s) {
-        return cities.stream().filter(city -> city.getCountryCode().equals(" " + s)).sorted(Comparator.comparing(City::getPopulation)).collect(Collectors.toList());
+        return cities.stream().filter(city -> city.getCountryCode().equals(" " +s)).sorted(Comparator.comparing(City::getPopulation)).collect(Collectors.toList()); }
 
-    }
+    public List<String> HighestCityPopPerCountry(String s,int order) {
+        return cities.stream()
+                .filter(city -> city.getCountryCode().equals(" " + s))
+                .sorted(Comparator.comparing(City::getPopulation).reversed())
+                .limit(order)
+                .map(City::getName)
+                .collect(Collectors.toList()); }
+
+    public List<String> HighestCapitalPop() {
+        List<City> capitals = new ArrayList<>();
+        List<Integer> capitalCodes = countries.stream()
+                .map(Country::getCapital)
+                .distinct().collect(Collectors.toList());
+        //List<City> capitals = this.cities.contains(capitalCodes); 
+        for(Integer code:capitalCodes) {
+            cities.stream()
+                    .filter(city -> city.getId()==(code))
+                    .forEach(city -> capitals.add(city)); }
+        return capitals.stream()
+                .sorted(Comparator.comparing(City::getPopulation).reversed())
+                .limit(1)
+                .map(City::getName)
+                .collect(Collectors.toList()); }
 }
--- StringFunctionalInterface/src/main/java/Main.java
+++ StringFunctionalInterface/src/main/java/Main.java
@@ -14,20 +14,24 @@ public class Main {
         List<Country> countries = countryReadCSV.readCountriesFromCSV("src/main/java/Countries.csv");
         //Methods Class
         CityCountryMethods cityCountryMethods=new CityCountryMethods(countries,cities);
+
         //Create a map that uses the country code as keys and a list of cities as the value for
-        //each country. 
+        //each country.
 
-
-        Map countryCities=new HashMap();
+        HashMap countryCities=new HashMap();
+        countryCities=cityCountryMethods.countryCities();
+        System.out.println(" Country:Cities\n" + countryCities);
 
         // For a given country code (from Console) sort the cities according to the population
-        List <City> citiesSorted=cityCountryMethods.sortedCitiesByCountryCode("EGY");
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter Country Code");
+        String s=scanner.nextLine();
+        List <City> citiesSorted=cityCountryMethods.sortedCitiesByCountryCode(s);
         for(City c:citiesSorted){
             System.out.println(c.getName()+" "+c.getPopulation());}
-
         //Get a List of countries population
         List<Integer> populations = cityCountryMethods.populations();
-        System.out.println(populations);
+        System.out.println("Populations list\n"+populations);
 
         //Get the average countries population
         Double average =cityCountryMethods.averageOfPopulations();
@@ -36,6 +40,14 @@ public class Main {
         //Get the maximum countries population
         Integer maximumPopulation=cityCountryMethods.maximumPopulation();
         System.out.println("Maximum Population is " + maximumPopulation);
+
+        //Highest City Population Per Country
+        List <String> highestCity=cityCountryMethods.HighestCityPopPerCountry("AFG",1);
+        System.out.println(highestCity);
+
+        //Highest Capital Population
+        List <String> highestCapitalPop=cityCountryMethods.HighestCapitalPop();
+        System.out.println(highestCapitalPop);
 
 
 
